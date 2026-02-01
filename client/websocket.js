7bc38b15daf2dc05ef5a86e51770624c5d93a724
Teja0412 <126227976+Teja0412@users.noreply.github.com>
2026-02-02 03:46:10 +0530

    Update websocket.js

--- client/websocket.js
+++ client/websocket.js
@@ -1,45 +1,27 @@
-// Connect to socket server
-const socket = io();
-
-// Store other users' cursor positions
-const remoteCursors = {};
-
-// 🔹 Receive drawing data from other users
-socket.on("draw", (data) => {
-  drawLine(
-    data.x1,
-    data.y1,
-    data.x2,
-    data.y2
-  );
+// CONNECT TO BACKEND
+const socket = io("https://collaborative-canvas-backend.onrender.com", {
+  transports: ["websocket"],
 });
-socket.on("redraw", (strokes) => {
-  console.log("REDRAW RECEIVED", strokes.length);
 
+// Receive drawing data
+socket.on("draw", (data) => {
+  drawLine(data.x1, data.y1, data.x2, data.y2);
+});
+
+socket.on("redraw", (strokes) => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   strokes.forEach((stroke) => {
     stroke.segments.forEach((seg) => {
-      drawLine(
-        seg.x1,
-        seg.y1,
-        seg.x2,
-        seg.y2
-      );
+      drawLine(seg.x1, seg.y1, seg.x2, seg.y2);
     });
   });
 });
 
-
-// 🔹 Receive cursor movement from other users
 socket.on("cursor_move", (data) => {
-  remoteCursors[data.id] = {
-    x: data.x,
-    y: data.y
-  };
+  remoteCursors[data.id] = { x: data.x, y: data.y };
 });
 
-// 🔹 Remove cursor when user disconnects
 socket.on("user_left", (data) => {
   delete remoteCursors[data.id];
 });
